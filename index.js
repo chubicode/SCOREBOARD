cfca96e64f73f0f60af5a557deb843aa3ff78cb0
Chubi <okafor.chinwuba@gmail.com>
2022-12-22 19:18:02 +0000

    reset
    Introduced the reset button

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ let scorehomeEl= document.getElementById("scorehome-el")
 let scoreguestEl= document.getElementById("scoreguest-el")
 
 let score= 0
-let score2= 0
-
-// for the home
-
 function plusone(){
   score=score + 1
   scorehomeEl.textContent=score
@@ -21,29 +17,17 @@ function plusthree(){
   scorehomeEl.textContent=score
 }
 
-// for the guest
-
 function guestplusone(){
-  score2=score2 + 1
-  scoreguestEl.textContent=score2
+  score=score + 1
+  scoreguestEl.textContent=score
 }
 
 function guestplustwo(){
-  score2=score2 + 2
-  scoreguestEl.textContent=score2
+  score=score + 2
+  scoreguestEl.textContent=score
 }
 
 function guestplusthree(){
-  score2=score2 + 3
-  scoreguestEl.textContent=score2
+  score=score + 3
+  scoreguestEl.textContent=score
 }
-
-// resetting home and guest counter
-
-function resethome(){
-score=0
-score2=0
-scorehomeEl.textContent=score
-scoreguestEl.textContent=score2
-}
-
